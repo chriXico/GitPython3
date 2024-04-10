4f31d3afa6d3656b71f9b9de2429ecd2448d0bb2
chriXico <158236914+chriXico@users.noreply.github.com>
2024-04-09 17:19:09 -0700

    Practica de inputs en colores

---
+++ GitPython/FirstAttempt.py
@@ -0,0 +1,5 @@
+nombre = input("Nombre: ")
+edad = input("Edad: ")
+email = input("Correo electronico: ")
+
+print("\033[31mMi nombre es " + nombre + ", \033[32mtengo " + edad + " años y se enviará un correo electrónico a la siguiente \033[33mdireccion " + email)
