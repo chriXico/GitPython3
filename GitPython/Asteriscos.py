8d63e6885dbbae730a2197f16bf3129c012ff8b0
Leche_Con_Hielo <158236914+chriXico@users.noreply.github.com>
2024-05-04 16:37:57 -0700

    Asteriscos.py

--- GitPython/Asteriscos.py
+++ GitPython/Asteriscos.py
@@ -1,13 +1,9 @@
-# Solicitar al usuario que ingrese la altura del triángulo
 altura = int(input("Ingrese la altura del triángulo: "))
 
-# Inicializar una variable para controlar el número de asteriscos a imprimir en cada línea
 asteriscos = 1
 
-# Bucle para generar las líneas del triángulo
 for i in range(altura):
-    # Imprimir la línea actual
+
     print('*' * asteriscos)
     
-    # Incrementar el número de asteriscos para la siguiente línea
     asteriscos += 1
